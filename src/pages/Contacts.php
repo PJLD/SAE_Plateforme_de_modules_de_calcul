4148a4e8c6b44806318ac233fb295304fc222cc5
PJLD <pierrejld2005@gmail.com>
2024-11-12 14:42:32 +0100

    modification du code du fichier Contacts.php

--- src/pages/Contacts.php
+++ src/pages/Contacts.php
@@ -1,58 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
-</html><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Contacts</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/moncss.css">
-</head>
-<body>
-<div class="header">
-    <img src="../images/Logo.png" alt="Logo du site" class="logo">
-    <h1>CALCUB</h1>
-</div>
-<nav>
-    <ol>
-        <li><a href="Accueil.php">Accueil</a></li>
-        <li><a href="ModuleDeCalcul.html">Module de Calcul</a></li>
-        <li><a href="CalculCSV.php">Calculer via CSV</a></li>
-        <li><a href="Contacts.html">Contacts</a></li>
-        <li><a href="Login.html"><button type="button" class="login">Login</button></a></li>
-    </ol>
-</nav>
-<h2>Voici une vidéo présentative de notre projet</h2>
-<div class="video-container">
-    <video width="600" controls autoplay muted>
-        <source src="../video/Video.mp4" type="video/mp4">
-        Votre navigateur ne supporte pas la balise vidéo.
-    </video>
-</div>
-<div style="height: 50px;"></div>
-<div class="contacts-list">
-    <h2 style="font-size: 20px;">Vous pouvez nous contacter via nos adresses mail respectives : </h2>
-    <ul>
-        <li>bilong.noa@gmail.com</li>
-        <li>c17esteban@gmail.com</li>
-        <li>lukds.dasilva@gmail.com</li>
-        <li>pierrejld2005@gmail.com</li>
-        <li>joseph.tramier1@gmail.com</li>
-    </ul>
-</div>
-<footer>
-    <h1 style="padding-top: 30px;">Membres de l'équipe</h1>
-    <p>INF2-FI-A</p>
-    <div style="padding-bottom: 30px;">
-        <p>Bilong Noa - Colombani Esteban - Da Silva Luca - Juillard Pierre - Tramier Joseph</p>
-    </div>
-</footer>
-</body>
-</html>
+<?php
+include("../templates/header.html");
+echo"<title>accueil</title></head>";
+echo"<body>";
+include("../templates/navbar.html");
+echo"<h2>Voici une vidéo présentative de notre projet</h2>";
+echo"<div class='video-container'>";
+echo"<video width='600' controls autoplay muted>";
+echo"<source src='../video/Video.mp4' type='video/mp4'></video>";
+echo"</div>";
+echo"<div style='height: 50px;'></div>";
+echo"<div class='contacts-list'>";
+echo"<h2 style='font-size: 20px;'>Vous pouvez nous contacter via nos adresses mail respectives : </h2>";
+echo"<ul>";
+echo"<li>bilong.noa@gmail.com</li>";
+echo"<li>c17esteban@gmail.com</li>";
+echo"<li>lukds.dasilva@gmail.com</li>";
+echo"<li>pierrejld2005@gmail.com</li>";
+echo"<li>joseph.tramier1@gmail.com</li>";
+echo"</ul>";
+echo"</div>";
+include("../templates/footer.html");
