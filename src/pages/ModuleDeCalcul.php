13112a197efdde19f99c77805fc6711e26d856c8
PJLD <pierrejld2005@gmail.com>
2024-11-12 14:45:26 +0100

    modification du code du fichier ModuleDeCalcul.php

--- src/pages/ModuleDeCalcul.php
+++ src/pages/ModuleDeCalcul.php
@@ -1,47 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Module de Calcul</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/moncss.css">
-</head>
-<body>
-<div class="header">
-    <img src="../images/Logo.png" alt="Logo du site" class="logo">
-    <h1>CALCUB</h1>
-</div>
-<nav>
-    <ol>
-        <li><a href="Accueil.php">Accueil</a></li>
-        <li><a href="ModuleDeCalcul.html">Module de Calcul</a></li>
-        <li><a href="CalculCSV.php">Calculer via CSV</a></li>
-        <li><a href="Contacts.php">Contacts</a></li>
-        <li><a href="Login.php"><button type="button" class="login">Login</button></a></li>
-    </ol>
-</nav>
-<h2>Veuillez saisir vos séries</h2>
-<form method='post'>
-    <label for="SerieA">Série a</label>
-    <input type='text' name='SerieA' id="SerieA" placeholder='1,2,3,...'>
-    <label for="SerieB">Série b</label>
-    <input type='text' name='SerieB' id="SerieB" placeholder='1,2,3,...'>
-    <button type='submit' name='OK' >OK</button>
-    <label for="calcul">Sélectionnez votre calcul</label>
-    <select name="calcul" id="calcul">
-        <option value="moyenne">Moyenne</option>
-        <option value="écarttype">Ecart-type</option>
-        <option value="esperance">Esperance</option>
-        <option value="variance">Variance</option>
-    </select>
-    <button type='submit' name='Calculer' >Calculer</button>
-</form>
-<footer>
-    <h1 style="padding-top: 30px;">Membres de l'équipe</h1>
-    <p>INF2-FI-A</p>
-    <div style="padding-bottom: 30px;">
-        <p>Bilong Noa - Colombani Esteban - Da Silva Luca - Juillard Pierre - Tramier Joseph</p>
-    </div>
-</footer>
-</body>
-</html>
+<?php
+include("../templates/header.html");
+echo"<title>accueil</title></head>";
+echo"<body>";
+include("../templates/navbar.html");
+echo"<h2>Veuillez saisir vos séries</h2>";
+echo"<form method='post'>";
+echo"<label for='SerieA'>Série a</label>";
+echo"<input type='text' name='SerieA' id='SerieA' placeholder='1,2,3,...'>";
+echo"<label for='SerieB'>Série b</label>";
+echo"<input type='text' name='SerieB' id='SerieB' placeholder='1,2,3,...'>";
+echo"<button type='submit' name='OK' >OK</button>";
+echo"<label for='calcul'>Sélectionnez votre calcul</label>";
+echo"<select name='calcul' id='calcul'>";
+echo"<option value='moyenne'>Moyenne</option>";
+echo"<option value='écarttype'>Ecart-type</option>";
+echo"<option value='esperance'>Esperance</option>";
+echo"<option value='variance'>Variance</option>";
+echo"</select>";
+echo"<button type='submit' name='Calculer' >Calculer</button>";
+echo"</form>";
+include("../templates/footer.html");
