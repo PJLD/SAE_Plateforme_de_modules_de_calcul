2f574552aad355ec736a9f39324904720ceba4fe
Esteban <c17esteban@gmail.com>
2024-11-12 16:15:40 +0100

    modification du code de Accueil.php

--- src/pages/Accueil.php
+++ src/pages/Accueil.php
@@ -1,20 +1,20 @@
 <?php
 include("../templates/header.html");
-echo"<title>accueil</title></head>";
-echo"<body>";
+echo"<title>accueil</title></head>
+<body>";
 include("../templates/navbar.html");
-echo"<div class='ImageAccueil'>";
-echo "<img src='../images/Image1.jpg' alt='Image Accueil 1'>";
-echo "<img src='../images/Image2.webp' alt='Image Accueil 2'>";
-echo "<img src='../images/Image3.jpg' alt='Image Accueil 3'>";
-echo"</div>";
-echo"<div style='height: 100px;'></div>";
-echo"<h2>Voici une vidéo présentative de notre projet</h2>";
-echo"<div class='video-container'>";
-echo"<video width='600' controls autoplay muted>";
-echo"<source src='../video/Video.mp4' type='video/mp4'></video>";
-echo"</div>";
-echo"<p>
+echo"<div class='ImageAccueil'>
+<img src='../images/Image1.jpg' alt='Image Accueil 1'>
+<img src='../images/Image2.webp' alt='Image Accueil 2'>
+<img src='../images/Image3.jpg' alt='Image Accueil 3'>
+</div>
+<div style='height: 100px;'></div>
+<h2>Voici une vidéo présentative de notre projet</h2>
+<div class='video-container'>
+<video width='600' controls autoplay muted>
+<source src='../video/Video.mp4' type='video/mp4'></video>
+</div>
+<p>
 Bonjour
 <p>";
 include("../templates/footer.html");
