43adeb1b90677eb6abe436e349957fff55c02e30
lucadsv <lukds.dasilva@gmail.com>
2024-11-12 15:16:04 +0100

    Déplacement de la vidéo de la page Contacts.php à la page Accueil.php

--- src/pages/Accueil.php
+++ src/pages/Accueil.php
@@ -9,6 +9,11 @@ echo "<img src='../images/Image2.webp' alt='Image Accueil 2'>";
 echo "<img src='../images/Image3.jpg' alt='Image Accueil 3'>";
 echo"</div>";
 echo"<div style='height: 100px;'></div>";
+echo"<h2>Voici une vidéo présentative de notre projet</h2>";
+echo"<div class='video-container'>";
+echo"<video width='600' controls autoplay muted>";
+echo"<source src='../video/Video.mp4' type='video/mp4'></video>";
+echo"</div>";
 echo"<p>
 Bonjour
 <p>";
