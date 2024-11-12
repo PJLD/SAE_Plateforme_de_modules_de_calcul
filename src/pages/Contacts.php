b773eb311303e3a18ad4dc872bcba35d6d17f0f7
Po-Ptitjo <joseph.tramier1@gmail.com>
2024-11-12 15:40:50 +0100

    Optimisation de l'utilisation des echo dans le fichier Contacts.php

--- src/pages/Contacts.php
+++ src/pages/Contacts.php
@@ -1,17 +1,17 @@
 <?php
 include("../templates/header.html");
-echo"<title>accueil</title></head>";
-echo"<body>";
+echo"<title>accueil</title></head>
+<body>";
 include("../templates/navbar.html");
-echo"<div style='height: 50px;'></div>";
-echo"<div class='contacts-list'>";
-echo"<h2 style='font-size: 20px;'>Vous pouvez nous contacter via nos adresses mail respectives : </h2>";
-echo"<ul>";
-echo"<li>bilong.noa@gmail.com</li>";
-echo"<li>c17esteban@gmail.com</li>";
-echo"<li>lukds.dasilva@gmail.com</li>";
-echo"<li>pierrejld2005@gmail.com</li>";
-echo"<li>joseph.tramier1@gmail.com</li>";
-echo"</ul>";
-echo"</div>";
+echo"<div style='height: 50px;'></div>
+<div class='contacts-list'>
+<h2 style='font-size: 20px;'>Vous pouvez nous contacter via nos adresses mail respectives : </h2>
+<ul>
+<li>bilong.noa@gmail.com</li>
+<li>c17esteban@gmail.com</li>
+<li>lukds.dasilva@gmail.com</li>
+<li>pierrejld2005@gmail.com</li>
+<li>joseph.tramier1@gmail.com</li>
+</ul>
+</div>";
 include("../templates/footer.html");
