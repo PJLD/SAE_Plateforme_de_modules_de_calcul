2cca77ff14a0ba3fb6727a365e6afcbfbc0b6d3b
lucadsv <lukds.dasilva@gmail.com>
2024-11-16 14:32:40 +0100

    Ajout de la fonction tableau($file) dans le fichier Fonctions.php

--- src/gestion/Fonctions.php
+++ src/gestion/Fonctions.php
@@ -1,2 +1,26 @@
 <?php
 //Fonctions nécessaires
+
+//fonction csv en tableau
+function tableau($file){
+    if (file_exists($file)) {
+        $fp = fopen($file, "r");
+        echo "<table class='tableau'>";
+        $res = fgetcsv($fp, 1024, ",");
+        echo "<tr>";
+        foreach ($res as $valeur) {
+            echo "<th>".$valeur."</th>";
+        }
+        echo "</tr>";
+        while ($res = fgetcsv($fp, 1024, ",")) {
+            echo "<tr>";
+            foreach ($res as $valeur) {
+                echo "<td>".$valeur."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        header("Location: CalculCSV.php?error");
+    }
+}
