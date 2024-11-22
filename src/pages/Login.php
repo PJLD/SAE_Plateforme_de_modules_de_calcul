81c3eddb1decf2e0b6258d5c4d70d93d0c51d5af
lucadsv <lukds.dasilva@gmail.com>
2024-11-22 11:30:38 +0100

    Traitement de la connexion dans le fichier Login.php

--- src/pages/Login.php
+++ src/pages/Login.php
@@ -12,4 +12,23 @@ echo"<h2>Veuillez vous connecter</h2>
 <button type='submit' name='Connexion' >Connexion</button>
 </form>
 <p><a href='SignIn.php'>Créer un compte</a></p>";
+
+if(isset($_POST['ok'])){
+    $login = htmlspecialchars($_POST['Login']);
+    $mdp = htmlspecialchars($_POST['Mdp']);
+    $mdp2 = md5($mdp);
+    $sql = "SELECT * FROM Comptes WHERE Login=? and MDP=?";
+    $cnx = mysqli_connect("localhost","root","");
+    $bd = mysqli_select_db($cnx, "SAES3");
+    $stmt = mysqli_prepare($cnx, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $login, $mdp2);
+    mysqli_stmt_execute($stmt);
+    $res = mysqli_stmt_get_result($stmt);
+    if(mysqli_num_rows($res) == 1){
+        echo "<p style='font-size: 50px;'>Login ok</p>";
+    } else {
+        echo "<p style='font-size: 50px;'>Login ou mot de passe incorrect</p>";
+    }
+}
+
 include("../templates/footer.html");
