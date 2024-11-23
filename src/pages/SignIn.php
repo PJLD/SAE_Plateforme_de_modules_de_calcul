c42aa15dae5ed6c61676e777f5dfb98d8718bd53
lucadsv <lukds.dasilva@gmail.com>
2024-11-23 17:17:41 +0100

    Correction du formulaire dans le fichier SignIn.php

--- src/pages/SignIn.php
+++ src/pages/SignIn.php
@@ -5,19 +5,11 @@ echo"<title>accueil</title></head>
 include("../templates/navbar.html");
 echo"<h2>Créer un compte</h2>
 <form method='post'>
-<label for='Nom'>Nom</label>
-<input type='text' name='Nom' id='Nom' placeholder='Nom'>
-<label for='Prenom'>Prénom</label>
-<input type='text' name='Prenom' id='Prenom' placeholder='Prénom'>
-<label for='Mail'>Mail</label>
-<input type='email' name='Mail' id='Mail' placeholder='Adresse mail'>
 <label for='Login'>Login</label>
 <input type='text' name='Login' id='Login' placeholder='Login'>
 <label for='Mdp'>Mot de passe</label>
 <input type='password' name='Mot de Passe' id='Mdp' placeholder='Mot de passe'>
-<label for='ConfirmerMdp'>Confirmer le mot de passe</label>
-<input type='password' name='Confirmer le Mot de Passe' id='ConfirmerMdp' placeholder='Mot de passe'>
-<button type='submit' name='Connexion' >Connexion</button>
+<button type='submit' name='Inscription'>S'inscrire</button>
 </form>
 <p><a href='Login.php'>J'ai déja un compte</a></p>";
 include("../templates/footer.html");
