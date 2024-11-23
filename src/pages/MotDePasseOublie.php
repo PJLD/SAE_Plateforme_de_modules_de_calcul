30113d671ae4ce7e069f17236d4173549f0b582a
lucadsv <lukds.dasilva@gmail.com>
2024-11-23 18:05:05 +0100

    Ajout du contenu de la page MotDePasseOublie.php

--- src/pages/MotDePasseOublie.php
+++ src/pages/MotDePasseOublie.php
@@ -1,1 +1,17 @@
 <?php
+include("../templates/header.html");
+echo"<title>Mot de Passe Oublie</title></head>
+<body>";
+include("../templates/navbar.html");
+echo"<div style='height: 80px;'></div>
+<div style='text-align: center; margin: 20px 0;'>
+    <img src='../images/Construction.png' alt='Image Construction Site Web'>
+</div>
+<div class='TexteExplicatif'>
+    <h1>Mot de passe oublié ?</h1>
+    <p>Cette page est actuellement en cours de construction. Nous travaillons activement pour vous offrir un service qui vous permettra de récupérer facilement votre mot de passe.</p>
+    <p>Revenez bientôt pour accéder à cette fonctionnalité.</p>
+    <p>Merci pour votre patience et votre compréhension.</p>
+    <p>(L'équipe de développement du site)</p>
+</div>";
+include("../templates/footer.html");
