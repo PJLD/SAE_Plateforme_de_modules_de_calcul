23bd15f448cb91cbdc9b09d275c1ea0ceebf4c67
Noa <bilong.noa@gmail.com>
2024-11-25 12:24:44 +0100

    ajout de la possibilité d'inscription (ajoute le nouvelle inscrit dans la table compte

--- src/pages/SignIn.php
+++ src/pages/SignIn.php
@@ -8,8 +8,33 @@ echo"<h2>Créer un compte</h2>
 <label for='Login'>Login</label>
 <input type='text' name='Login' id='Login' placeholder='Login'>
 <label for='Mdp'>Mot de Passe</label>
-<input type='password' name='Mot de Passe' id='Mdp' placeholder='Mot de passe'>
+<input type='password' name='Mdp' id='Mdp' placeholder='Mot de passe'>
 <button type='submit' name='Inscription'>S'inscrire</button>
 </form>
 <p><a href='Login.php'>J'ai déja un compte</a></p>";
+
+
+if (isset($_POST["Inscription"])) {
+    $Login = $_POST["Login"];
+    $Mdp = $_POST["Mdp"];
+    $mdp2 = md5($Mdp);
+
+    $cnx = mysqli_connect("localhost", "root", "");
+    $bd = mysqli_select_db($cnx, "SAE");
+
+    $sql = "INSERT INTO  Comptes (Login, Mdp) VALUES (?, ?)";
+
+    $stmt = mysqli_prepare($cnx, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $Login, $mdp2);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo "<p>Inscription réussie !</p>";
+    } else {
+        echo "<p>Erreur lors de l'inscription.</p>";
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($cnx);
+}
+
 include("../templates/footer.html");
