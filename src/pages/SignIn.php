d5fc333e16357d48908ff050840da4fad198729f
Esteban <c17esteban@gmail.com>
2024-11-25 15:55:38 +0100

    ajout de captcha à l'inscription

--- src/pages/SignIn.php
+++ src/pages/SignIn.php
@@ -3,12 +3,22 @@ include("../templates/header.html");
 echo"<title>Sign In</title></head>
 <body>";
 include("../templates/navbar.html");
+
+$elem1 = rand(1, 10);
+$elem2 = rand(1, 10);
+
+$valeur_captcha = $elem1 * $elem2;
+
+setcookie('captcha', $valeur_captcha, time() + 1800, "/");
+
 echo"<h2>Créer un compte</h2>
 <form method='post'>
 <label for='Login'>Login</label>
 <input type='text' name='Login' id='Login' placeholder='Login'>
 <label for='Mdp'>Mot de Passe</label>
 <input type='password' name='Mdp' id='Mdp' placeholder='Mot de passe'>
+<label for='captcha'>$elem1 * $elem2</label>
+<input type='text' name='captcha' id='captcha' placeholder='Résultat de l opération'>
 <button type='submit' name='Inscription'>S'inscrire</button>
 </form>
 <p><a href='Login.php'>J'ai déja un compte</a></p>";
@@ -18,6 +28,7 @@ if (isset($_POST["Inscription"])) {
     $Login = $_POST["Login"];
     $Mdp = $_POST["Mdp"];
     $mdp2 = md5($Mdp);
+    $captcha = htmlspecialchars($_POST['captcha']);
 
     $cnx = mysqli_connect("localhost", "root", "");
     $bd = mysqli_select_db($cnx, "SAE");
@@ -28,7 +39,11 @@ if (isset($_POST["Inscription"])) {
     mysqli_stmt_bind_param($stmt, "ss", $Login, $mdp2);
 
     if (mysqli_stmt_execute($stmt)) {
-        echo "<p>Inscription réussie !</p>";
+        if (isset($_COOKIE['captcha']) && $captcha == $_COOKIE['captcha']) {
+            echo "<p style='font-size: 50px;'>Inscription réussie !</p>";
+        } else {
+            echo "<p style='font-size: 50px;'>Captcha incorrect</p>";
+        }
     } else {
         echo "<p>Erreur lors de l'inscription.</p>";
     }
