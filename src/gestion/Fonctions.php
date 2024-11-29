8d1a9b850f6c8c2aafb7d17628d1c6ec5ed010ff
Po-Ptitjo <joseph.tramier1@gmail.com>
2024-11-29 10:57:22 +0100

    Ajout de la fonction qui calcule la densité de probabilité de la loi inverse gaussienne dans le fichier Fonctions.php

--- src/gestion/Fonctions.php
+++ src/gestion/Fonctions.php
@@ -56,3 +56,18 @@ function ecartType($serie, $probabilites){
     $variance = variance($serie, $probabilites);
     return sqrt($variance);
 }
+// fonction pour calculer la densité de probabiblité de la loi inverse gaussienne
+function loiInverseGaussienne($mu, $lambda) {
+    if ($mu <= 0 || $lambda <= 0) {
+        throw new InvalidArgumentException("Les paramètres mu et lambda doivent être positifs.");
+    }
+
+    $p = mt_rand() / mt_getrandmax();
+    $q = mt_rand() / mt_getrandmax();
+
+    $y = sqrt(-2 * log($p));
+
+    $x1 = $mu + $mu * $mu / (2 * $lambda) * ($y + sqrt($y * $y + 4 * $lambda * $mu));
+
+    return $x1;
+}
