ccae56379955bcd09a9f00cb6806df7affb32fca
lucadsv <lukds.dasilva@gmail.com>
2024-12-06 14:18:45 +0100

    Ajout de la page Historique.php et de son contenu

---
+++ src/pages/Historique.php
@@ -0,0 +1,71 @@
+<?php
+include("../templates/header.html");
+echo "<title>Historique</title>
+<style>
+    /* Styles pour le tableau */
+    table {
+        width: 70%;
+        border-collapse: collapse;
+        margin: auto;
+        font-size: 18px;
+    }
+
+    th, td {
+        border: 1px solid #ddd;
+        padding: 15px;
+        text-align: center;
+    }
+
+    th {
+        background-color: #1c305f;
+        color: white;
+        font-weight: bold;
+    }
+
+    tr:hover {
+        background-color: #ddd;
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+</style>
+</head>
+<body>";
+include("../templates/navbar.html");
+
+$cnx = mysqli_connect("localhost", "root", "");
+$bd = mysqli_select_db($cnx, "SAE");
+
+$sql = "SELECT * FROM Historique";
+$resultat = mysqli_query($cnx, $sql);
+
+echo "<h1 style='text-align: center; color: #1c305f; margin-top: 80px; margin-bottom: 80px;'>Historique des Calculs</h1>";
+
+echo "<table>";
+
+$lignes = mysqli_fetch_assoc($resultat);
+
+if ($lignes) {
+    echo "<tr>";
+    foreach ($lignes as $key => $value) {
+        echo "<th>$key</th>";
+    }
+    echo "</tr>";
+
+    do {
+        echo "<tr>";
+        foreach ($lignes as $value) {
+            echo "<td>$value</td>";
+        }
+        echo "</tr>";
+    } while ($lignes = mysqli_fetch_assoc($resultat));
+} else {
+    echo "<tr><td colspan='100%' style='text-align: center;'>Aucun historique trouvé.</td></tr>";
+}
+echo "</table>";
+
+mysqli_close($cnx);
+
+include("../templates/footer.html");
