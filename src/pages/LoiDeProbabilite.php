eaed6dc64aa1cf0b22706aa25d4fc09cab39f21d
Po-Ptitjo <joseph.tramier1@gmail.com>
2024-12-06 11:51:57 +0100

    Mise en place du formulaire,de l'appel de la fonction qui calcule l'integral a l'aide de la methode des trapezes ainsi que de l'affichage du résultat

--- src/pages/LoiDeProbabilite.php
+++ src/pages/LoiDeProbabilite.php
@@ -1,26 +1,53 @@
 <?php
 include("../templates/header.html");
+require_once("../gestion/Fonctions.php");
 echo"<title>Loi de Probabilité</title></head>
 <body>";
 include("../templates/navbar.html");
 
 echo"<h2>Loi Inverse-Gaussienne</h2>
 <form method='post' style='max-width: 25%;'>
-<label for='Esperance'>L'espérance μ</label>
-<input type='text' name='Esperance' id='Esperance' placeholder='μ'>
-<label for='Forme'>La forme λ</label>
-<input type='text' name='Forme' id='Forme' placeholder='λ'>
-<label for='T'>La valeur t <=> P(X≤t) où X suit la loi inverse-gaussienne</label>
-<input type='text' name='T' id='T' placeholder='t'>
-<label for='NbValeurs'>Le nombre de valeur prise sur l’intervalle n≥1</label>
-<input type='text' name='NbValeurs' id='NbValeurs' placeholder='Nombre de valeurs'>
-<label for='méthode'>Sélectionnez votre méthode de calcul</label>
-<select name='méthode' id='méthode'>
-<option value='Méthode1'>Méthode 1</option>
-<option value='Méthode2'>Méthode 2</option>
-<option value='Méthode3'>Méthode 3</option>
+<label for='mu'>L'espérance μ</label>
+<input type='text' name='mu' id='mu' placeholder='μ'>
+<label for='lambda'>La forme λ</label>
+<input type='text' name='lambda' id='lambda' placeholder='λ'>
+<label for='a'>Valeur de la borne inférieure a (avec a > 0) </label>
+<input type='number' name='a' id='a' placeholder='a'>
+<label for='b'>Valeur de la borne supérieure b (avec b > 0 et b > a) </label>
+<input type='number' name='b' id='b' placeholder='b'>
+<label for='n'>Le nombre de sous intervalles, pour la méthode des trapezes</label>
+<input type='number' name='n' id='n' placeholder='n'>
+<label for='methode'>Sélectionnez votre méthode de calcul</label>
+<select name='methode' id='methode'>
+<option value='Méthode des trapezes'>Méthode des trapezes</option>
 </select>
 <button type='submit' name='Calculer' >Calculer</button>
 </form>";
+
+if (isset($_POST['Calculer'])) {
+    $mu = $_POST['mu'];
+    $lambda = $_POST['lambda'];
+    $a = $_POST['a'];
+    $b = $_POST['b'];
+    $n = $_POST['n'];
+
+    $calcul = $_POST['methode'];
+    $message= null;
+
+    $result = trapezoidal_integration('loiInverseGaussienne', $a, $b, $lambda, $mu,$n);
+
+
+
+    echo "<div class='resultatConteneur'>";
+    if ($message) {
+        echo "<h3>Résultat du calcul : $calcul</h3>
+              <p><strong>$message</strong></p>";
+    } else {
+        echo "<h3>Résultat du calcul : $calcul</h3>
+              <p><strong>$result</strong></p>";
+    }
+    echo "</div>";
+
+}
 
 include("../templates/footer.html");
