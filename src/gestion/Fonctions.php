3ccdc348f9bd75e448ac08870806742058ac7ce9
PJLD <pierrejld2005@gmail.com>
2025-01-27 11:19:03 +0100

    suppression de la fonction tableau au fichier Fonctions.php

--- src/gestion/Fonctions.php
+++ src/gestion/Fonctions.php
@@ -15,27 +15,6 @@ function gererNavBar(){
     }
 }
 
-//fonction csv en tableau
-function tableau($file){
-    if (file_exists($file)) {
-        $fp = fopen($file, "r");
-        echo "<table class='tableau'>";
-        $res = fgetcsv($fp, 1024, ";");
-        echo "<tr>";
-        foreach ($res as $valeur) {
-            echo "<th>".$valeur."</th>";
-        }
-        echo "</tr>";
-        while ($res = fgetcsv($fp, 1024, ";")) {
-            echo "<tr>";
-            foreach ($res as $valeur) {
-                echo "<td>".$valeur."</td>";
-            }
-            echo "</tr>";
-        }
-        echo "</table>";
-    }
-}
 
 //fonction pour calculer la moyenne
 function moyenne($serie){
