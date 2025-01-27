06cc47236cb6c4d6e673a21fd5df1e95049826e9
lucadsv <lukds.dasilva@gmail.com>
2025-01-27 11:03:39 +0100

    Amélioration de la visibilité du code du fichier Historique.php

--- src/pages/Historique.php
+++ src/pages/Historique.php
@@ -2,7 +2,9 @@
 session_start();
 require_once("../gestion/Fonctions.php");
 include("../templates/header.html");
-echo "<title>Historique</title>
+
+echo "
+<title>Historique</title>
 <style>
     /* Styles pour le tableau */
     table {
@@ -30,26 +32,25 @@ echo "<title>Historique</title>
 </style>
 </head>
 <body>";
+
 gererNavBar();
 
 $cnx = mysqli_connect("localhost", "sae", "sae");
 $bd = mysqli_select_db($cnx, "SAE");
 $login = $_SESSION['login'];
-
 $sql = "SELECT * FROM Historique WHERE Login = '$login'";
 $resultat = mysqli_query($cnx, $sql);
 
 echo "<h1 style='text-align: center; color: #1c305f; margin-top: 80px; margin-bottom: 80px;'>Historique des Calculs</h1>";
-
-echo"<form method='post' style='all: unset; display: flex; justify-content: center; margin-bottom: 50px;'>
-<button type='submit' name='SupprimerHistorique' style='font-family: cursive; padding: 5px; max-height: 80px; width: 14%; font-size: 14px; background-color: darkred; color: white; border: none; padding: 5px; margin-top: 5px; cursor: pointer;'>Supprimer mon historique</button>
+echo"
+<form method='post' style='all: unset; display: flex; justify-content: center; margin-bottom: 50px;'>
+    <button type='submit' name='SupprimerHistorique' style='font-family: cursive; padding: 5px; max-height: 80px; width: 14%; font-size: 14px; background-color: darkred; color: white; border: none; padding: 5px; margin-top: 5px; cursor: pointer;'>Supprimer mon historique</button>
 </form>";
 
+//suppression de l'historique de l'utilisateur
 if (isset($_POST['SupprimerHistorique'])) {
     $cnx = mysqli_connect("localhost", "sae", "sae");
     $bd = mysqli_select_db($cnx, "SAE");
-
-
     $suppressionHistorique = "DELETE FROM Historique WHERE Login = ?";
     $stmt = mysqli_prepare($cnx, $suppressionHistorique);
     $login = $_SESSION['login'];
@@ -60,9 +61,7 @@ if (isset($_POST['SupprimerHistorique'])) {
 }
 
 echo "<table>";
-
 $lignes = mysqli_fetch_assoc($resultat);
-
 if ($lignes) {
     echo "<tr>";
     foreach ($lignes as $key => $value) {
