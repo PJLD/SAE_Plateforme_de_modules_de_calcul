9662cfa04b90409b6b78eb26e6a343efe73f9297
lucadsv <lukds.dasilva@gmail.com>
2025-01-27 11:03:26 +0100

    Amélioration de la visibilité du code du fichier Contacts.php

--- src/pages/Contacts.php
+++ src/pages/Contacts.php
@@ -2,18 +2,25 @@
 session_start();
 require_once("../gestion/Fonctions.php");
 include("../templates/header.html");
-echo"<title>Contacts</title></head>
+
+echo"
+<title>Contacts</title>
+</head>
 <body>";
+
 gererNavBar();
-echo"<div style='height: 50px;'></div>
+
+echo"
+<div style='height: 50px;'></div>
 <div class='contacts-list'>
-<h2 style='font-size: 20px;'>Vous pouvez nous contacter via nos adresses mail respectives : </h2>
-<ul>
-<li>bilong.noa@gmail.com</li>
-<li>c17esteban@gmail.com</li>
-<li>lukds.dasilva@gmail.com</li>
-<li>pierrejld2005@gmail.com</li>
-<li>joseph.tramier1@gmail.com</li>
-</ul>
+    <h2 style='font-size: 20px;'>Vous pouvez nous contacter via nos adresses mail respectives : </h2>
+    <ul>
+        <li>bilong.noa@gmail.com</li>
+        <li>c17esteban@gmail.com</li>
+        <li>lukds.dasilva@gmail.com</li>
+        <li>pierrejld2005@gmail.com</li>
+        <li>joseph.tramier1@gmail.com</li>
+    </ul>
 </div>";
+
 include("../templates/footer.html");
