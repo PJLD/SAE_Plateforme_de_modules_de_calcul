b9f9f7e682ea4bd50942517192f02e866b790e56
Po_Ptitjo <joseph.tramier1@gmail.com>
2025-02-11 14:34:00 +0100

    Vérification de la comformité des valeurs, ainsi que de leur affichage dans le fichier LoiDeProbabilite.php

--- src/pages/LoiDeProbabilite.php
+++ src/pages/LoiDeProbabilite.php
@@ -44,12 +44,10 @@ echo"
         <input type='number' name='mu' id='mu' placeholder='μ' required>
     <label for='lambda'>La forme λ</label>
         <input type='number' name='lambda' id='lambda' placeholder='λ' required>
-    <label for='a'>Valeur de la borne inférieure a (avec a > 0)</label>
-        <input type='number' name='a' id='a' placeholder='a' min='1' required>
-    <label for='t'>t tel que P(X ≤ t) où X suit la loi inverse-gaussienne et t > a</label>
-        <input type='number' name='t' id='t' placeholder='t' min='1' required>
+    <label for='t'>t tel que P(X ≤ t) où X suit la loi inverse-gaussienne</label>
+        <input type='number' name='t' id='t' placeholder='t' required>
     <label for='n'>Le nombre de sous intervalles (avec n > 0)</label>
-        <input type='number' name='n' id='n' placeholder='n' min='1' required>
+        <input type='number' name='n' id='n' placeholder='n' required>
     <label for='methode'>Sélectionnez votre méthode de calcul</label>
         <select name='methode' id='methode'>
             <option value='Methode des trapezes'>Méthode des trapezes</option>
@@ -61,12 +59,12 @@ echo"
 
 //traitement du calcul (bouton 'Calculer')
 if (isset($_POST['Calculer'])) {
-    $a=0;
+    $result="";
+    $a=0.1;
     $mu = $_POST['mu'];
     $lambda = $_POST['lambda'];
     $b = $_POST['t']; //correspond au champ de t
     $n = $_POST['n'];
-    $a=$_POST['a'];
     $calcul = $_POST['methode'];
 
     $login = $_SESSION['login'];
@@ -74,68 +72,84 @@ if (isset($_POST['Calculer'])) {
     $cnx = mysqli_connect("localhost", "sae", "sae");
     $bd = mysqli_select_db($cnx, "SAE");
 
-    if ($calcul =='Methode des trapezes') {
-        $result = methodeDesTrapezes($a, $b, $mu, $lambda,$n);
-        $moyenne = calculerXBarreTrapeze($a,$b,$mu,$lambda,$n);
-    }elseif ($calcul =='Methode des rectangles') {
-        $result = methodeDesRectangles($a, $b, $mu, $lambda,$n);
-        $moyenne = calculerXBarreRectangles($a,$b,$mu,$lambda,$n);
-    }elseif ($calcul =='Methode de Simpson') {
-        $result = methodeDeSimpson($a, $b,$mu,$lambda,$n);
-        $moyenne=calculerXBarreSimpson($a,$b,$mu,$lambda,$n);
+    if ($b <= 0 || $n < 0 || $mu < 0 || $lambda < 0) {
+        $result=  "Vérifer que tout vos parametres soit bien postif";
+        echo "<div class='resultatConteneur'>";
+        echo "<h3>Résultat du calcul : $calcul</h3>
+        <p><strong>$result</strong></p>";
+        echo "</div>";
+    }elseif ($calcul == 'Methode de Simpson' && $n%2==1){
+        $result= "Vous avez choisi la méthode de Simpson, donc ajuster un nombre de sous-intervalles pair";
+        echo "<div class='resultatConteneur'>";
+        echo "<h3>Résultat du calcul : $calcul</h3>
+        <p><strong>$result</strong></p>";
+        echo "</div>";
     }
-    $sigma = sqrt(pow($mu,3) / $lambda);
 
-    $resultats = [
-        'Résultat' => $result,
-        'Lambda' => $lambda,
-        'Moyenne'=> $moyenne,
-        'Ecart-type' => $sigma,
-        'Méthode' => $calcul
-    ];
 
-    echo "<table class='tableau-resultats'>";
-    echo "<tbody>";
-    foreach ($resultats as $parametre => $valeur) {
-        echo "<tr>";
-        echo "<td>" . $parametre . "</td>";
-        echo '<td>' . $valeur . "</td>";
-        echo "</tr>";
-    }
-    echo "</tbody>";
-    echo "</table>";
+    else{
+        if ($calcul =='Methode des trapezes') {
+            $result = methodeDesTrapezes($a, $b, $mu, $lambda,$n);
+            $moyenne = calculerXBarreTrapeze($a,$b,$mu,$lambda,$n);
+        }elseif ($calcul =='Methode des rectangles') {
+            $result = methodeDesRectangles($a, $b, $mu, $lambda,$n);
+            $moyenne = calculerXBarreRectangles($a,$b,$mu,$lambda,$n);
+        }elseif ($calcul =='Methode de Simpson') {
+            $result = methodeDeSimpson($a, $b,$mu,$lambda,$n);
+            $moyenne=calculerXBarreSimpson($a,$b,$mu,$lambda,$n);
+        }
+        $sigma = sqrt(pow($mu,3) / $lambda);
 
-    echo "<div class='resultatConteneur'>";
-    echo "<h3>Résultat du calcul : $calcul</h3>
+        $resultats = [
+            'Résultat' => $result,
+            'Lambda' => $lambda,
+            'Moyenne'=> $moyenne,
+            'Ecart-type' => $sigma,
+            'Méthode' => $calcul
+        ];
+
+        echo "<table class='tableau-resultats'>";
+        echo "<tbody>";
+        foreach ($resultats as $parametre => $valeur) {
+            echo "<tr>";
+            echo "<td>" . $parametre . "</td>";
+            echo '<td>' . $valeur . "</td>";
+            echo "</tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
+
+        echo "<div class='resultatConteneur'>";
+        echo "<h3>Résultat du calcul : $calcul</h3>
     <p><strong>$result</strong></p>";
-    $sql = "INSERT INTO Historique (login, date, calcul, resultat) VALUES (?, ?, ?, ?)";
-    $stmt = mysqli_prepare($cnx, $sql);
-    mysqli_stmt_bind_param($stmt, "sssd", $login, $date, $calcul, $result);
-    if (mysqli_stmt_execute($stmt)) {
-        echo "<p style='color: green; text-align: center;'>Le résultat a été enregistré dans votre historique</p>";
-    } else {
-        echo "<p style='color: red; text-align: center;'>Erreur lors de l'enregistrement du résultat dans votre historique</p>";
-    }
-    echo "</div>";
+        $sql = "INSERT INTO Historique (login, date, calcul, resultat) VALUES (?, ?, ?, ?)";
+        $stmt = mysqli_prepare($cnx, $sql);
+        mysqli_stmt_bind_param($stmt, "sssd", $login, $date, $calcul, $result);
+        if (mysqli_stmt_execute($stmt)) {
+            echo "<p style='color: green; text-align: center;'>Le résultat a été enregistré dans votre historique</p>";
+        } else {
+            echo "<p style='color: red; text-align: center;'>Erreur lors de l'enregistrement du résultat dans votre historique</p>";
+        }
+        echo "</div>";
 
-    // Calcul des points pour le graphique
-    $points_x = [];
-    $points_y = [];
-    $inter = ($b - $a) / $n;
-    for ($i = 0; $i <= $n; $i++) {
-        $x = $a + $i * $inter; //x
-        $y = loiInverseGaussienne($x, $lambda, $mu); //f(x)
-        $points_x[] = $x;
-        $points_y[] = $y;
-    }
+        // Calcul des points pour le graphique
+        $points_x = [];
+        $points_y = [];
+        $inter = ($b - $a) / $n;
+        for ($i = 0; $i <= $n; $i++) {
+            $x = $a + $i * $inter; //x
+            $y = loiInverseGaussienne($x, $lambda, $mu); //f(x)
+            $points_x[] = $x;
+            $points_y[] = $y;
+        }
 
-    // Affichage du graphique
-    echo "
+        // Affichage du graphique
+        echo "
     <div style='display: flex; justify-content: center; margin-top: 30px;'>
         <canvas id='graphCanvas' style='width: 600px; height: 400px;'></canvas>
     </div>";
 
-    echo "
+        echo "
     <script src='https://cdn.jsdelivr.net/npm/chart.js'></script> <!--importation de chart.js-->
     <script>
     var ctx = document.getElementById('graphCanvas'); 
@@ -173,6 +187,7 @@ if (isset($_POST['Calculer'])) {
     });
     </script>
     ";
+    }
 }
 
 include("../templates/footer.html");
