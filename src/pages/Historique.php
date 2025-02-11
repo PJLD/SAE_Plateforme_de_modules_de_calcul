e1a9c322a22b2cd00eee35bacab382a0913d9d3e
lucadsv <lukds.dasilva@gmail.com>
2025-02-11 15:08:59 +0100

    Ajout de la suppression de chaque ligne de l'historique

--- src/pages/Historique.php
+++ src/pages/Historique.php
@@ -38,8 +38,10 @@ gererNavBar();
 $cnx = mysqli_connect("localhost", "sae", "sae");
 $bd = mysqli_select_db($cnx, "SAE");
 $login = $_SESSION['login'];
-$sql = "SELECT * FROM Historique WHERE Login = '$login'";
+$sql = "SELECT Login, DateHistorique, Calcul, Resultat FROM Historique WHERE Login = '$login' ORDER BY STR_TO_DATE(DateHistorique, '%d/%m/%Y %H:%i:%s') DESC";
+$sqlID = "SELECT * FROM Historique WHERE Login = '$login'";
 $resultat = mysqli_query($cnx, $sql);
+$resultatID = mysqli_query($cnx, $sqlID);
 
 echo "<h1 style='text-align: center; color: #1c305f; margin-top: 80px; margin-bottom: 80px;'>Historique des Calculs</h1>";
 echo"
@@ -62,11 +64,13 @@ if (isset($_POST['SupprimerHistorique'])) {
 
 echo "<table>";
 $lignes = mysqli_fetch_assoc($resultat);
+$lignesID = mysqli_fetch_assoc($resultatID);
 if ($lignes) {
     echo "<tr>";
     foreach ($lignes as $key => $value) {
         echo "<th>$key</th>";
     }
+    echo "<th>Supprimer Historique</th>";
     echo "</tr>";
 
     do {
@@ -74,12 +78,27 @@ if ($lignes) {
         foreach ($lignes as $value) {
             echo "<td>$value</td>";
         }
+        echo "<td><a href='?deleteHistorique=" . $lignesID['ID'] . "' class='delete-link'>Supprimer</a></td>";
         echo "</tr>";
     } while ($lignes = mysqli_fetch_assoc($resultat));
 } else {
     echo "<tr><td colspan='100%' style='text-align: center;'>Aucun historique trouvé.</td></tr>";
 }
 echo "</table>";
+
+//Suppression d'utilisateurs
+if (isset($_GET['deleteHistorique'])) {
+    $id = $_GET['deleteHistorique'];
+    $suppressionHistorique = "DELETE FROM Historique WHERE ID = ?";
+    $stmt = mysqli_prepare($cnx, $suppressionHistorique);
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    if (mysqli_stmt_execute($stmt)){
+        header("Location: Historique.php");
+    } else {
+        echo "<p style='color: red; text-align: center;'>Erreur lors de la suppression.</p>";
+    }
+    mysqli_stmt_close($stmt);
+}
 
 mysqli_close($cnx);
 
