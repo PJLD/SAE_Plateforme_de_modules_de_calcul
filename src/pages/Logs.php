b73a3d538bd3ac235d028a868aa803b68ad6c3a0
Esteban <c17esteban@gmail.com>
2025-03-26 11:12:50 +0100

    Ajout de bouton pour télécharger les logs en json

--- src/pages/Logs.php
+++ src/pages/Logs.php
@@ -1,47 +1,69 @@
 <?php
 session_start();
+
+$cnx = mysqli_connect("localhost", "sae", "sae", "SAE");
+if (!$cnx) {
+    die("Erreur de connexion : " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM Logs ORDER BY STR_TO_DATE(Date, '%d/%m/%Y %H:%i:%s') DESC;";
+$resultat = mysqli_query($cnx, $sql);
+
+if (isset($_POST['download_json'])) {
+    header('Content-Type: application/json');
+    header('Content-Disposition: attachment; filename="logs.json"');
+    $logs = [];
+    while ($row = mysqli_fetch_assoc($resultat)) {
+        $logs[] = $row;
+    }
+    echo json_encode($logs, JSON_PRETTY_PRINT);
+    mysqli_close($cnx);
+    exit();
+}
+
+$resultat = mysqli_query($cnx, $sql);
+
 require_once("../gestion/Fonctions.php");
 include("../templates/header.html");
 
 echo"
 <title>Logs</title>
 <style>
-    table {
-        width: 70%;
-        border-collapse: collapse;
-        margin: auto;
-        font-size: 18px;
-    }
+   table {
+       width: 70%;
+       border-collapse: collapse;
+       margin: auto;
+       font-size: 18px;
+   }
 
-    th, td {
-        border: 1px solid #ddd;
-        padding: 15px;
-        text-align: center;
-    }
+   th, td {
+       border: 1px solid #ddd;
+       padding: 15px;
+       text-align: center;
+   }
 
-    th {
-        background-color: #1c305f;
-        color: white;
-        font-weight: bold;
-    }
+   th {
+       background-color: #1c305f;
+       color: white;
+       font-weight: bold;
+   }
 
-    tr:hover {
-        background-color: #ddd;
-    }
+   tr:hover {
+       background-color: #ddd;
+   }
 </style>
 </head>
 <body>";
 
 gererNavBar();
 
-$cnx = mysqli_connect("localhost", "sae", "sae");
-$bd = mysqli_select_db($cnx, "SAE");
-$login = $_SESSION['login'];
-$sql = "SELECT * FROM Logs 
-ORDER BY STR_TO_DATE(Date, '%d/%m/%Y %H:%i:%s') DESC;";
-$resultat = mysqli_query($cnx, $sql);
-
 echo "<h1 style='text-align: center; color: #1c305f; margin-top: 80px; margin-bottom: 80px;'>Base des Logs</h1>";
+
+echo "
+<form method='post'>
+   <button type='submit' name='download_json'>Télécharger les logs en JSON</button>
+</form>
+";
 
 echo "<table>";
 $lignes = mysqli_fetch_assoc($resultat);
@@ -67,3 +89,4 @@ echo "</table>";
 mysqli_close($cnx);
 
 include("../templates/footer.html");
+?>
